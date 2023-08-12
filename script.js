c43649d5d727501f47f37955f6516ca84185cbe1
DJ007WEB <007officialjyo@gmail.com>
2023-08-12 17:06:14 +0530

    navigation bar complete

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+
+const header = document.querySelector(".header");
+
+
+const toggleNavBar = () => {
+    header.classList.toggle("active")
+}
+
+
+mobile_nav.addEventListener("click", () => toggleNavBar());
